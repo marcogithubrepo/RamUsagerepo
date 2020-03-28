738714f42b9972976572a1cede5c6f57e82737ef
marco <rizzato.marco.rm@gmail.com>
2020-03-28 11:16:13 +0100

    1.2 add form item autoresize

--- RamUsage/Form1.Designer.cs
+++ RamUsage/Form1.Designer.cs
@@ -36,11 +36,15 @@
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label2 = new System.Windows.Forms.Label();
             this.listViewRam = new System.Windows.Forms.ListView();
-            this.lblRamInfo = new System.Windows.Forms.Label();
             this.listViewCpu = new System.Windows.Forms.ListView();
             this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.chartRAMPerc = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.chartCPUPerc = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.lblRamInfo = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chartRAMPerc)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chartCPUPerc)).BeginInit();
             this.SuspendLayout();
@@ -64,92 +68,142 @@
             // 
             this.listViewRam.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.listViewRam.HideSelection = false;
-            this.listViewRam.Location = new System.Drawing.Point(543, 54);
+            this.listViewRam.Location = new System.Drawing.Point(566, 80);
             this.listViewRam.Name = "listViewRam";
-            this.listViewRam.Size = new System.Drawing.Size(223, 107);
+            this.listViewRam.Size = new System.Drawing.Size(207, 110);
             this.listViewRam.TabIndex = 7;
             this.listViewRam.UseCompatibleStateImageBehavior = false;
-            // 
-            // lblRamInfo
-            // 
-            this.lblRamInfo.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.lblRamInfo.AutoSize = true;
-            this.lblRamInfo.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblRamInfo.Location = new System.Drawing.Point(607, 20);
-            this.lblRamInfo.Name = "lblRamInfo";
-            this.lblRamInfo.Size = new System.Drawing.Size(95, 22);
-            this.lblRamInfo.TabIndex = 8;
-            this.lblRamInfo.Text = "RAM Info:";
             // 
             // listViewCpu
             // 
             this.listViewCpu.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.listViewCpu.HideSelection = false;
-            this.listViewCpu.Location = new System.Drawing.Point(543, 280);
+            this.listViewCpu.Location = new System.Drawing.Point(566, 320);
             this.listViewCpu.Name = "listViewCpu";
-            this.listViewCpu.Size = new System.Drawing.Size(223, 107);
+            this.listViewCpu.Size = new System.Drawing.Size(208, 110);
             this.listViewCpu.TabIndex = 10;
             this.listViewCpu.UseCompatibleStateImageBehavior = false;
             // 
             // label1
             // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(608, 242);
+            this.label1.Location = new System.Drawing.Point(3, 240);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(93, 22);
+            this.label1.Size = new System.Drawing.Size(557, 30);
             this.label1.TabIndex = 11;
-            this.label1.Text = "CPU Info:";
+            this.label1.Text = "CPU Chart:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 71.89441F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 28.10559F));
+            this.tableLayoutPanel1.Controls.Add(this.label4, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.chartRAMPerc, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblRamInfo, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.listViewRam, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.listViewCpu, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.chartCPUPerc, 0, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(784, 481);
+            this.tableLayoutPanel1.TabIndex = 13;
             // 
             // chartRAMPerc
             // 
-            this.chartRAMPerc.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.chartRAMPerc.BackColor = System.Drawing.SystemColors.Control;
+            this.chartRAMPerc.BackSecondaryColor = System.Drawing.SystemColors.Control;
             chartArea1.Name = "ChartArea1";
             this.chartRAMPerc.ChartAreas.Add(chartArea1);
-            this.chartRAMPerc.Location = new System.Drawing.Point(10, 9);
-            this.chartRAMPerc.MinimumSize = new System.Drawing.Size(521, 100);
+            this.chartRAMPerc.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chartRAMPerc.Location = new System.Drawing.Point(3, 33);
+            this.chartRAMPerc.MinimumSize = new System.Drawing.Size(520, 150);
             this.chartRAMPerc.Name = "chartRAMPerc";
             series1.ChartArea = "ChartArea1";
             series1.Name = "Series1";
             this.chartRAMPerc.Series.Add(series1);
-            this.chartRAMPerc.Size = new System.Drawing.Size(521, 197);
-            this.chartRAMPerc.TabIndex = 10;
+            this.chartRAMPerc.Size = new System.Drawing.Size(557, 204);
+            this.chartRAMPerc.TabIndex = 11;
             this.chartRAMPerc.Text = "chart1";
             // 
             // chartCPUPerc
             // 
-            this.chartCPUPerc.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.chartCPUPerc.BackColor = System.Drawing.SystemColors.Control;
             chartArea2.Name = "ChartArea1";
             this.chartCPUPerc.ChartAreas.Add(chartArea2);
-            this.chartCPUPerc.Location = new System.Drawing.Point(10, 235);
-            this.chartCPUPerc.MinimumSize = new System.Drawing.Size(521, 100);
+            this.chartCPUPerc.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chartCPUPerc.Location = new System.Drawing.Point(3, 273);
+            this.chartCPUPerc.MinimumSize = new System.Drawing.Size(520, 150);
             this.chartCPUPerc.Name = "chartCPUPerc";
             series2.ChartArea = "ChartArea1";
             series2.Name = "Series1";
             this.chartCPUPerc.Series.Add(series2);
-            this.chartCPUPerc.Size = new System.Drawing.Size(521, 197);
-            this.chartCPUPerc.TabIndex = 12;
+            this.chartCPUPerc.Size = new System.Drawing.Size(557, 205);
+            this.chartCPUPerc.TabIndex = 13;
             this.chartCPUPerc.Text = "chartcpu";
+            // 
+            // lblRamInfo
+            // 
+            this.lblRamInfo.AutoSize = true;
+            this.lblRamInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblRamInfo.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRamInfo.Location = new System.Drawing.Point(3, 0);
+            this.lblRamInfo.Name = "lblRamInfo";
+            this.lblRamInfo.Size = new System.Drawing.Size(557, 30);
+            this.lblRamInfo.TabIndex = 8;
+            this.lblRamInfo.Text = "RAM Chart:";
+            this.lblRamInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(566, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(215, 30);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "RAM info:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(566, 240);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(215, 30);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "CPU Info:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(804, 483);
-            this.Controls.Add(this.chartCPUPerc);
-            this.Controls.Add(this.chartRAMPerc);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.listViewCpu);
-            this.Controls.Add(this.lblRamInfo);
-            this.Controls.Add(this.listViewRam);
+            this.ClientSize = new System.Drawing.Size(784, 481);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MaximumSize = new System.Drawing.Size(800, 700);
+            this.MinimumSize = new System.Drawing.Size(800, 450);
             this.Name = "Form1";
             this.Text = "Ram Usage";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chartRAMPerc)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chartCPUPerc)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -160,11 +214,14 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ListView listViewRam;
-        private System.Windows.Forms.Label lblRamInfo;
         private System.Windows.Forms.ListView listViewCpu;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         public System.Windows.Forms.DataVisualization.Charting.Chart chartRAMPerc;
         public System.Windows.Forms.DataVisualization.Charting.Chart chartCPUPerc;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblRamInfo;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- RamUsage/Form1.cs
+++ RamUsage/Form1.cs
@@ -207,5 +207,6 @@ namespace RamUsage
             return true;
 
         }
+
     }
 }
